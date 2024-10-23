710ee573b4db504fff09b00fa8f746011ed72903
habildem <habildmrc@gmail.com>
2024-10-23 18:29:51 +0300

    Swift-Travel-Book

---
+++ TravelBookTests/TravelBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBookTests.swift
+//  TravelBookTests
+//
+//  Created by Habil Demirci on 14.10.2024.
+//
+
+import Testing
+@testable import TravelBook
+
+struct TravelBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
